9a4bd5db738d6a222158f429c5369eec0ee00797
soheila <soheilalotfi@ymail.com>
2019-07-07 20:15:56 -0400

    track how many people are connected to the app

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -57,7 +57,40 @@ database.ref('/data').on("value", function(snapshot){
         $("#highest-price").text(highPrice);
     }
   
-})
+});
+
+// track how many people are connected through firbase 
+
+// connectionsRef references a specific location in our database.
+// All of our connections will be stored in this directory.
+
+var connectionsRef = database.ref("/connections");
+
+// '.info/connected' is a special location provided by Firebase that is updated
+// every time the client's connection state changes.
+// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+var connectedRef = database.ref(".info/connected");
+
+// When the client's connection state changes...
+connectedRef.on("value", function(snap) {
+
+  // If they are connected..
+  if (snap.val()) {
+
+    // Add user to the connections list.
+    var con = connectionsRef.push(true);
+    // Remove user from the connection list when they disconnect.
+    con.onDisconnect().remove();
+  }
+});
+
+// When first loaded or when the connections list changes...
+connectionsRef.on("value", function(snap) {
+
+  // Display the viewer count in the html.
+  // The number of online users is the number of children in the connections list.
+  $("#connected-viewers").text(snap.numChildren());
+});
 
 
 
